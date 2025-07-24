a34da191fbb01a85f854a410851920cfd74caaa6
pizzetegl <contilorenzo@gmail.com>
2025-07-24 19:46:19 +0200

    fime connessione cambiato

--- src/DB_CONFIG_FILE.php
+++ src/DB_CONFIG_FILE.php
@@ -3,8 +3,8 @@
 // update with your real credentials. Then set the path in the DB_CONFIG_FILE
 // environment variable or define the individual DB_* variables.
 return [
-    'db_host' => 'localhost',
-    'db_name' => 'example_db',
-    'db_user' => 'example_user',
-    'db_pass' => 'secret',
+    'db_host' => '31.11.39.237',
+    'db_name' => 'Sql1412226_2',
+    'db_user' => 'Sql1412226',
+    'db_pass' => '6i5067256h',
 ];
